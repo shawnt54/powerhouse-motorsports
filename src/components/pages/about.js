d1f688aadfebae4ab121922ed446e915519f4391
shawnt54 <shawndthurlow@gmail.com>
2022-03-02 14:31:16 -0700

    updated bio image

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,5 +1,5 @@
 import React from "react";
-import profilePicture from "../../../static/assets/images/bio/headshot.jpg";
+import profilePicture from "../../../static/assets/images/bio/headshot1.jpg";
 
 export default function() {
   return (
